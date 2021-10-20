89f06bbca69b4d0ab46e120e72f4490665fec49a
Preshen Naidoo <Preshen.Naidoo@absa.africa>
2021-10-20 15:55:03 +0200

    wired up base network kit, viewmodel to fetch data

---
+++ StackOverFlow/StackOverFlow/Main Screen/StackOverFlowViewController.swift
@@ -0,0 +1,43 @@
+//
+//  StackOverFlowViewController.swift
+//  StackOverFlow
+//
+//  Created by Preshen Naidoo on 2021/10/20.
+//
+
+import UIKit
+import SOFComponentsKit
+import SOFNetworkKit
+
+class StackOverFlowViewController: UIViewController {
+    
+    @IBOutlet private var listview: ListView!
+    
+    private lazy var viewModel = StackOverFlowViewModel(interactor: BaseNetworkLayer(),
+                                                        delegate: self)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.fetch()
+    }
+}
+
+extension StackOverFlowViewController: BaseViewModelDelegate {
+    func refreshViewContents() {
+        //TODO
+    }
+    
+    func showErrorMessage(errorMessage: String) {
+        //TODO
+    }
+}
+
+extension StackOverFlowViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //TODO
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //TODO
+    }
+}
