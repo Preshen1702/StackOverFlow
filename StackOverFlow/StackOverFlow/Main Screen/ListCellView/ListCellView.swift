ab93b25228388580cc3763a6d68b58fcea544d08
Preshen Naidoo <Preshen.Naidoo@absa.africa>
2021-10-21 10:22:15 +0200

    Search bar color is correct and made the search bar component

---
+++ StackOverFlow/StackOverFlow/Main Screen/ListCellView/ListCellView.swift
@@ -0,0 +1,20 @@
+//
+//  ListCellView.swift
+//  StackOverFlow
+//
+//  Created by Preshen Naidoo on 2021/10/20.
+//
+
+import Foundation
+import UIKit
+import SOFComponentsKit
+
+class ListCellView: UITableViewCell {
+    
+ 
+    @IBOutlet private var listInputView: ListView!
+    
+    func populate(with content: ListContentModel) {
+        listInputView.populate(with: content)
+    }
+}
--- StackOverFlow/StackOverFlow/Main Screen/StackOverFlowViewController.swift
+++ StackOverFlow/StackOverFlow/Main Screen/StackOverFlowViewController.swift
@@ -11,20 +11,39 @@ import SOFNetworkKit
 
 class StackOverFlowViewController: UIViewController {
     
-    @IBOutlet private var listview: ListView!
+    @IBOutlet private var tableView: UITableView!
+    @IBOutlet private var searchBarView: SearchBarView!
+    @IBOutlet private var loadingIndicator: UIActivityIndicatorView!
+    @IBOutlet private var loadingIndicatorView: UIView!
+    @IBOutlet private var centreViewHeading: UILabel!
     
     private lazy var viewModel = StackOverFlowViewModel(interactor: BaseNetworkLayer(),
                                                         delegate: self)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewModel.fetch()
+        self.setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        searchBarView.delegate = self
+        tableView.separatorStyle = .none
     }
 }
 
 extension StackOverFlowViewController: BaseViewModelDelegate {
     func refreshViewContents() {
-        //TODO
+        DispatchQueue.main.async {
+            self.loadingIndicator.stopAnimating()
+            self.loadingIndicatorView.isHidden = true
+            self.tableView.reloadData()
+            if self.viewModel.hasNoContent {
+                self.centreViewHeading.isHidden = false
+                self.loadingIndicatorView.isHidden = false
+            }
+        }
     }
     
     func showErrorMessage(errorMessage: String) {
@@ -34,12 +53,34 @@ extension StackOverFlowViewController: BaseViewModelDelegate {
 
 extension StackOverFlowViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //TODO
-        return 0
+        return viewModel.numberOfRows
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //TODO
-        return UITableViewCell()
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "ListCellView", for: indexPath) as? ListCellView
+        cell?.populate(with: viewModel.questionCard(at: indexPath.row))
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+}
+
+extension StackOverFlowViewController: SearchBarViewDelegate {
+    
+    func clearSearchBar() {
+        searchBarView.clearSearchBar()
+        loadingIndicator.startAnimating()
+        self.loadingIndicatorView.isHidden = false
+        self.centreViewHeading.isHidden = true
+        viewModel.searchStackOverFlow(with: "")
+    }
+    
+    func tappedSearchBar(with keyword: String) {
+        loadingIndicator.startAnimating()
+        self.loadingIndicatorView.isHidden = false
+        self.centreViewHeading.isHidden = true
+        viewModel.searchStackOverFlow(with: keyword)
     }
 }
